6ab7e83deb442e4dca201033160d3512450ce622
Alexander Tyutyunnikov <a.tyutyunnikov@gmail.com>
2016-01-25 12:37:34 +0300

    DDB-208 add config

---
+++ config.bat
@@ -0,0 +1,44 @@
+@echo off
+rem =======  Database connection settings  ==================================
+
+rem DB User, Password and Role for connection
+rem 
+rem This user must have enought privileges for retrieving data from required 
+rem tables.
+rem For more information see readme.txt
+rem
+set dbservername=TEMP\TEMP
+set dbname=temp_db_name
+set dbuser=temp_db_user
+set dbpass=temp_db_pass
+
+
+rem Full path to 'sqlcmd' tool of MS SQL - command line client for MS SQL
+set "sql_cmd_path=C:\Program Files\Microsoft SQL Server\100\Tools\Binn\"
+
+
+rem =======  FTP upload settings  ===========================================
+
+rem FTP host & port, user and password settings
+rem FTP host examples:
+rem   ftphost=ftp.docdoc.ru
+rem   ftphost=ftp.docdoc.ru:21
+set ftphost=ftp.docdoc.ru
+set ftpuser=testuser
+set ftppass=testpassword
+set ftpstartdir=testdir
+rem path to winscp ftp client
+set "winscppath=winscp\"
+set ftplogpath=ftp.log
+
+
+
+rem =======  Internal variables (DO NOT MODIFY THEM if not sure)  ===========
+
+rem Subpath to sql commandlets
+set sqlpath=.\sql\
+rem Subpath where to put export files
+set exportpath=.\export\
+rem List of subnames of sql commandlets (and corresponding export files)
+rem set exportfiles=clinics doctors depts doctschedule schedule
+set exportfiles=clinics doctors
