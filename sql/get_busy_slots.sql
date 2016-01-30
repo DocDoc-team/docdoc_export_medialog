b27cebc27c9ca4108311fe64600a7e879e4f491c
Alexander Tyutyunnikov <a.tyutyunnikov@gmail.com>
2016-01-30 09:53:46 +0300

    DDB-208 add выгружаю совещания

--- sql/get_busy_slots.sql
+++ sql/get_busy_slots.sql
@@ -1,15 +1,22 @@
 set nocount on;
-select 'doctor_id;clinic_id;date;time;duration';
+select 'doctor_id;clinic_id;date_start;date_finish';
 
 SELECT
-  schedule.MEDECINS_ID  doctor_id,
-  schedule.FM_INTORG_ID clinic_id,
-  DATE_CONS,
-  HEURE                 time,
-  DUREE                 duration
-FROM PLANNING
-  JOIN PL_SUBJ schedule ON schedule.PL_SUBJ_ID = PLANNING.PL_SUBJ_ID
-WHERE DATE_CONS BETWEEN DATEADD(DAY, DATEDIFF(DAY, 0, GETDATE()), 0) AND DATEADD(DAY, 30, GETDATE())
-      AND CANCELLED != 1
-      AND STATUS != 1
-ORDER BY PLANNING_ID DESC;
+   schedule.MEDECINS_ID  doctor_id,
+   schedule.FM_INTORG_ID clinic_id,
+   DATE_START,
+   DATE_END
+ FROM PLANNING
+   JOIN PL_SUBJ schedule ON schedule.PL_SUBJ_ID = PLANNING.PL_SUBJ_ID
+ WHERE DATE_START BETWEEN GETDATE() AND DATEADD(DAY, 30, GETDATE())
+       AND CANCELLED != 1
+       AND STATUS != 1
+ union all
+ select
+   schedule.MEDECINS_ID  doctor_id,
+   schedule.FM_INTORG_ID clinic_id,
+   DATE_START,
+   DATE_END
+ from PL_EXCL
+   JOIN PL_SUBJ schedule ON schedule.PL_SUBJ_ID = PL_EXCL.PL_SUBJ_ID
+ WHERE DATE_START BETWEEN GETDATE() AND DATEADD(DAY, 30, GETDATE())
